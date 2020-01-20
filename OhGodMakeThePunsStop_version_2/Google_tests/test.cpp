3ce7d2b5a1bf66dc118da5363e800ab672fbded9
William Howell <wlm.howell@gmail.com>
2020-01-20 02:56:06 -0500

    Added GoogleTest

---
+++ OhGodMakeThePunsStop_version_2/Google_tests/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "../src/Utilities.h"
+
+TEST(DoesGTestWork, simple) {
+	ASSERT_EQ(1, 1);
+}
--- OhGodMakeThePunsStop_version_2/cmake-build-debug/cmake_install.cmake
+++ OhGodMakeThePunsStop_version_2/cmake-build-debug/cmake_install.cmake
@@ -32,6 +32,12 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/Users/whowell/GitHub_Repositories/Oh-God-Make-The-Puns-Stop-2/OhGodMakeThePunsStop_version_2/cmake-build-debug/Google_tests/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
